023a828fe2e7b74f541017c9a9ca4c6cfef813d6
Nat Weerawan <nat.wrw@gmail.com>
2015-02-12 10:42:13 +0700

    Update opencv

--- web.js
+++ web.js
@@ -4,6 +4,7 @@ var express = require('express')
   , io = require('socket.io')
   , http = require('http')
   , morgan = require("morgan");
+var cv = require("opencv");
 
 var app = express()
     .use(morgan('combined'))
@@ -30,11 +31,61 @@ io.on('connection', function (socket) {
     console.log(data);
   });
 
-  
-  socket.on('frame', function (data) {
-		console.log("GOT FRAME: ");
-    //console.log("FRAME: ", data);
-  });
+	socket.on('frame', function(data) {
+      var orig_base64 = data;
+      // console.log(data.length);
+      //chrome 2459
+      // firefore 2727
+      if (!data || data.length < 5000) { 
+        return ; 
+      }
+
+      // 'data:image/jpeg;base64,'
+      data = data.split(',')[1]
+      var frame_data = new Buffer(data, 'base64'); 
+      cv.readImage(frame_data, function(err, im) {
+        var img_gray = im.copy();
+        var img_hsv = im.copy();
+        var filter_image = im.copy();
+
+        im.detectObject(cv.FACE_CASCADE, {}, function(err, faces){
+          if (!faces){
+            console.log("No Faces")
+            return;
+          }
+          var face = faces[0] || {}
+            , ims = im.size()
+
+          var im2 = im.roi(face.x, face.y, face.width, face.height)
+          
+          im.adjustROI(
+               -face.y
+             , (face.y + face.height) - ims[0]
+             , -face.x
+             , (face.x + face.width) - ims[1])
+             
+          img_gray.convertGrayscale();
+          img_hsv.convertHSVscale();
+
+          socket.emit('face_data', {
+            image_gray: img_gray.toBuffer().toString('base64'),
+            image_hsv: img_hsv.toBuffer().toString('base64'),
+            image_face: im.toBuffer().toString('base64'),
+            image_orig: orig_base64
+          })
+
+          socket.broadcast.emit('face_data', { 
+            image_gray: img_gray.toBuffer().toString('base64'), 
+            image_hsv: img_hsv.toBuffer().toString('base64'),
+            image_face: im.toBuffer().toString('base64'),
+            image_orig: orig_base64 
+          })
+
+          im2.save('out.jpg')
+
+        });
+      });
+    });
 
 });
 
